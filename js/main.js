ec31e592a273091060a5c335b08467a40ebdb08a
Pierre J. Callies <35848450+usa85@users.noreply.github.com>
2018-07-12 13:15:18 -0500

    with right banner wow

--- js/main.js
+++ js/main.js
@@ -143,7 +143,7 @@ TABLE OF CONTENTS:
     var element = $(".element");
     $(function() {
       element.typed({
-        strings: ["a Web Designer.", "a UI/UX Designer.", "a Photographer." ],
+        strings: ["a Web Designer.", "a Full Stack Developer.", "a Data Analyst.", " a Geographic Information System Specialist.", "a Project Manager.", " French.", "American." ],
         typeSpeed: 50,
         loop: true,
       });
